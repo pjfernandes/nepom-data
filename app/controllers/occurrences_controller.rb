350455237c44eaa0b8be206cec52ad7199b3afb0
Luis Pret <lcpret@gmail.com>
2021-11-08 14:24:20 -0300

    occurrences and ships

--- app/controllers/occurrences_controller.rb
+++ app/controllers/occurrences_controller.rb
@@ -1,8 +1,40 @@
 class OccurrencesController < ApplicationController
 
   skip_before_action :authenticate_user!, only: :index
+  # before_action :get_ship
 
   def index
     @occurrences = Occurrence.all
+  end
+
+  # def show
+
+  # end
+
+  # def new
+  #   @occurrence = Occurrence.new
+  # end
+
+  # def create
+  #   @occurrence = Occurrence.new(occurrence_params)
+  #   @occurrence.ship = @ship
+  #   @occurrence.user = current_user
+  #   if @occurrence.save
+  #     redirect_to root
+  #   else
+  #     render :new
+  #   end
+  # end
+
+
+
+
+
+
+
+  private
+
+  def get_ship
+    @ship = Ship.find(params[:ship_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'occurrences#index'
-  resources :ships , only: [ :index, :show, :new, :create, :edit, :update, :destroy ]
-  resources :occurrences
+  root to: 'ships#index'
+  resources :ships, only: %i[index show new create edit update destroy] do
+    resources :occurrences, only: :index
+  end
 end
