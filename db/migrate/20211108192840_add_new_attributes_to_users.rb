d0ce0d81354b3b3f122991ea461986b22e91b32a
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2021-11-08 17:06:27 -0300

    user pages frontend added

---
+++ db/migrate/20211108192840_add_new_attributes_to_users.rb
@@ -0,0 +1,8 @@
+class AddNewAttributesToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :cpf, :string
+    add_column :users, :matricula, :string
+    add_column :users, :lotacao, :string
+  end
+end
