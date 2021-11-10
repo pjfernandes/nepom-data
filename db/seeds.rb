99fa0f3c1792bb595d9698c34902ec0d6c3f571e
Luis Pret <lcpret@gmail.com>
2021-11-10 14:16:49 -0300

    seed ship

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ship = Ship.new
+ship.name = %w[Calipso Kontiki Pequod Pinta Palestrante Revenge].sample
+ships = Ship.create(
+  [
+    { name: 'Pinta', registration: '23456', registration_port: 'Hamburg' },
+    { name: 'Kontiki', registration: '23098', registration_port: 'Santos' },
+    { name: 'Calipso', registration: '45678', registration_port: 'Salvador' },
+    { name: 'Pequod', registration: '98765', registration_port: 'Nantucket' },
+    { name: 'Palestrante', registration: '34521', registration_port: 'Rio de Janeiro' },
+    { name: 'Revenge', registration: '76543', registration_port: 'Paranaguá' }
+  ]
+)
+''
