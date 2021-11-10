a82e75cbf10f8c6dced9089c0235c05c532242a5
Tatiane Matuchack <tatianematuchack@yahoo.com.br>
2021-11-10 10:40:49 -0300

    added allow blank validation to registration

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -3,5 +3,5 @@ class Ship < ApplicationRecord
   has_many :occurrences
   has_one_attached :photo
   validates :name, presence: true
-  validates :registration, uniqueness: true
+  validates :registration, uniqueness: true, allow_blank: true
 end
