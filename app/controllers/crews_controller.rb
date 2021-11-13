86b69eeb8b112332dfe9f0322381b3fdce68817f
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2021-11-12 22:05:05 -0300

    front-end-adjusted

--- app/controllers/crews_controller.rb
+++ app/controllers/crews_controller.rb
@@ -1,37 +1,14 @@
-
 class CrewsController < ApplicationController
 
   def index
-
-    ship_id_temp = params[:ship_id]
-
-    if ship_id_temp != nil
-      @ship_id = ship_id_temp
+    @crews = Crew.all
+    if params[:member_id] != nil
+      @member = Member.find(params[:member_id])
     end
-
-    sql_query_initial = "ship_id = #{@ship_id} AND member_id IS NOT NULL AND date_ini IS NOT NULL AND date_fin IS NOT NULL"
-    sql_query_date_ini = ""
-    sql_query_date_fin = ""    
-
-    if params[:query_date_ini].present?       
-      sql_query_date_ini = " AND date_ini >= '#{ Date.parse(params[:query_date_ini]) }' "
-    end
-
-    if params[:query_date_fin].present?
-      sql_query_date_fin = " AND date_fin <= '#{ Date.parse(params[:query_date_fin]) }' "
-    end
-
-    sql_query_final = sql_query_initial + sql_query_date_ini + sql_query_date_fin
-
-   
-    
-    @crews = Crew.joins(:member).where(sql_query_final)
-
-    if params[:query_name].present?
-      @crews = @crews.where( "name ILIKE ?", "%#{params[:query_name]}%")
+    if params[:ship_id] != nil
+      @ship = Ship.find(params[:ship_id])
     end
   end
-
 
   def new
     @crew = Crew.new()
