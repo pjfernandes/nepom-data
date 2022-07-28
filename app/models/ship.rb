7503ec0f4cd28120174e22a3147753f8c041d832
LC Pret <lcpret@gmail.com>
2022-07-28 16:12:04 -0300

    pg_search crews and ships

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -5,4 +5,11 @@ class Ship < ApplicationRecord
   has_one_attached :photo
   validates :name, presence: true
   validates :registration, uniqueness: true, allow_blank: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_ship,
+    against: %i[name registration registration_port],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
